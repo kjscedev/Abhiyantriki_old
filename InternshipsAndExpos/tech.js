bdd548e729edf0404c38269dd3ca0c6faf6252e4
twilight-warlock <devansh.ts@somaiya.edu>
2020-10-22 15:52:49 +0530

    Made template for videos responsive

--- InternshipsAndExpos/tech.js
+++ InternshipsAndExpos/tech.js
@@ -1,4 +1,8 @@
-document.getElementById('indro').addEventListener('click', function() {
-    this.innerHTML = '<iframe src="https://www.youtube.com/embed/a6KGPBflhiM?rel=0&autoplay=1" frameborder="0" allowfullscreen="false">';
-    this.className = 'video-embed';
-  });
+// document.getElementById('indro').addEventListener('click', function() {
+//     this.innerHTML = '<iframe src="https://www.youtube.com/embed/a6KGPBflhiM?rel=0&autoplay=1" frameborder="0" allowfullscreen="false">';
+//     this.className = 'video-embed';
+//   });
+// document.getElementById('name-2').addEventListener('click', function() {
+//     this.innerHTML = '<iframe src="https://www.youtube.com/embed/eoHC_rfX50U?rel=0&autoplay=1" frameborder="0" allowfullscreen="false">';
+//     this.className = 'video-embed order-1 order-md-2';
+//   });
