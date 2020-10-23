2779a38a1cf7a552c056d1ef6094f0ce707606f5
twilight-warlock <devansh.ts@somaiya.edu>
2020-10-23 10:18:18 +0530

    11 videos added

--- InternshipsAndExpos/tech.js
+++ InternshipsAndExpos/tech.js
@@ -1,8 +1,44 @@
-// document.getElementById('indro').addEventListener('click', function() {
-//     this.innerHTML = '<iframe src="https://www.youtube.com/embed/a6KGPBflhiM?rel=0&autoplay=1" frameborder="0" allowfullscreen="false">';
-//     this.className = 'video-embed';
-//   });
-// document.getElementById('name-2').addEventListener('click', function() {
-//     this.innerHTML = '<iframe src="https://www.youtube.com/embed/eoHC_rfX50U?rel=0&autoplay=1" frameborder="0" allowfullscreen="false">';
-//     this.className = 'video-embed order-1 order-md-2';
-//   });
+document.getElementById('indro').addEventListener('click', function() {
+    this.innerHTML = '<iframe src="https://www.youtube.com/embed/DOhUEYLIiHE?rel=0&autoplay=1" frameborder="0" allowfullscreen="false">';
+    this.className = 'video-embed order-1 order-md-2';
+  });
+document.getElementById('name-1').addEventListener('click', function() {
+    this.innerHTML = '<iframe src="https://drive.google.com/file/d/13RS4yNQxJizB2Gc_Okv1-vLlshdIMf3A/preview" >';
+    this.className = 'video-embed';
+  });
+document.getElementById('name-2').addEventListener('click', function() {
+    this.innerHTML = '<iframe src="https://drive.google.com/file/d/1m5W5HBFQM9RrjMa1CKd7dQ5f20rBfkBw/preview" >';
+    this.className = 'video-embed order-1 order-md-2';
+  });
+document.getElementById('name-3').addEventListener('click', function() {
+    this.innerHTML = '<iframe src="https://drive.google.com/file/d/19wg0D51Ksm36XQoCXxkffapb4gz62QzS/preview">';
+    this.className = 'video-embed';
+  });
+document.getElementById('name-4').addEventListener('click', function() {
+    this.innerHTML = '<iframe src="https://drive.google.com/file/d/1KpebNMmhPz7OZeHm3RNpx4T6TbS9WCbQ/preview">';
+    this.className = 'video-embed order-1 order-md-2';
+  });
+document.getElementById('name-5').addEventListener('click', function() {
+    this.innerHTML = '<iframe src="https://drive.google.com/file/d/1WoZmpYSEuYVMWSitjkSK6tqS2n64Fdfi/preview">';
+    this.className = 'video-embed';
+  });
+document.getElementById('name-6').addEventListener('click', function() {
+    this.innerHTML = '<iframe src="https://drive.google.com/file/d/1C31Qxq_7qxeZTo6sR-O_uGDr3RebcTVP/preview">';
+    this.className = 'video-embed order-1 order-md-2';
+  });
+document.getElementById('name-7').addEventListener('click', function() {
+    this.innerHTML = '<iframe src="https://drive.google.com/file/d/1-8iH2frVmL4sK-vF4uBRaHkwQHFNZTTO/preview">';
+    this.className = 'video-embed';
+  });
+document.getElementById('name-8').addEventListener('click', function() {
+    this.innerHTML = '<iframe src="https://drive.google.com/file/d/1rGwFrVYkckCTeISUvAxO3kaYL01paB2O/preview">';
+    this.className = 'video-embed order-1 order-md-2';
+  });
+document.getElementById('name-9').addEventListener('click', function() {
+    this.innerHTML = '<iframe src="https://drive.google.com/file/d/1-mmUccokeDvaDNgHMik6pUYFU1NuDVPw/preview">';
+    this.className = 'video-embed';
+  });
+document.getElementById('name-10').addEventListener('click', function() {
+    this.innerHTML = '<iframe src="https://drive.google.com/file/d/1VTV65xaDeYIoBKOIXvqYLAKx49aSfSvA/preview">';
+    this.className = 'video-embed order-1 order-md-2';
+  });
