70d700daa16f1f14c860a6336318426a501e945f
kjscelive <30316956+kjscedev@users.noreply.github.com>
2021-09-30 15:57:27 +0530

    removed 2021

--- js/page.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".exit-btn").forEach((btn) => {
-  btn.onclick = function () {
-    element = document.querySelector("#topScreen");
-    element.scrollIntoView({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
-});
